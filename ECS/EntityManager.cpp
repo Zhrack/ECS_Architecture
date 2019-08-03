b851d20f00ec27ffecd084e0cca93c3b41bcacf8
DESKTOP-DAVIDE\Davide <zanarchan@gmail.com>
2019-08-03 10:05:45 +0200

    component management functions

--- ECS/EntityManager.cpp
+++ ECS/EntityManager.cpp
@@ -1,6 +1,8 @@
 #include "pch.h"
 #include "EntityManager.h"
 
+#include <cassert>
+
 
 EntityManager::EntityManager(unsigned long entityPool, sf::RenderWindow* win):
     mComponents(CompType::COMP_COUNT),
@@ -21,6 +23,11 @@ EntityManager::~EntityManager()
 {
 }
 
+void EntityManager::update(float elapsed)
+{
+    mRenderProcessor.update(elapsed);
+}
+
 std::vector<Dependency> EntityManager::intersection(const std::vector<CompType>& deps)
 {
     std::vector<Dependency> result;
@@ -28,8 +35,6 @@ std::vector<Dependency> EntityManager::intersection(const std::vector<CompType>&
     auto depsSize = deps.size();
 
     // simple implementation for now, also deps size will be < 10 most of the time, pratically O(n)
-    ;
-
     for (auto entIDIter = mEntityIDPool.getUsedIDsIter();
         entIDIter != mEntityIDPool.endUsedIDsIter();
         entIDIter++)
@@ -55,3 +60,57 @@ std::vector<Dependency> EntityManager::intersection(const std::vector<CompType>&
 
     return result;
 }
+
+EntityID EntityManager::createEntity()
+{
+    auto id = mEntityIDPool.generateID();
+
+    // TODO notify processors
+
+    return id;
+}
+
+EntityID EntityManager::createEntity(const std::vector<CompType>& deps)
+{
+    auto id = createEntity();
+
+    // TODO add components and notify processors of new components
+
+    return id;
+}
+
+void EntityManager::removeEntity(EntityID id)
+{
+    mEntityIDPool.freeID(id);
+
+    // TODO notify processors
+}
+
+void EntityManager::removeComponent(CompType type, EntityID id)
+{
+    if (mComponents[type][id] == nullptr)
+    {
+        std::cout << "Component could not be removed, already nullptr!" << std::endl;
+        return;
+    }
+
+    mComponents[type][id].reset(nullptr);
+
+    // TODO notify processors of component
+
+}
+
+std::vector<std::pair<CompType, BaseComponent*>> EntityManager::getComponents(EntityID id)
+{
+    std::vector<std::pair<CompType, BaseComponent*>> result;
+
+    for (int t = 0; t < CompType::COMP_COUNT; ++t)
+    {
+        auto p = std::make_pair((CompType)t, mComponents[t][id].get());
+        result.push_back(p);
+    }
+
+    return result;
+}
+
+
--- ECS/EntityManager.h
+++ ECS/EntityManager.h
@@ -2,6 +2,7 @@
 #define ENTITY_MANAGER_H_
 
 #include <stack>
+#include <iostream>
 
 #include "EntityIDPool.h"
 
@@ -16,7 +17,29 @@ public:
     EntityManager(unsigned long entityPool, sf::RenderWindow* win);
     ~EntityManager();
 
+    void update(float elapsed);
+
     std::vector<Dependency> intersection(const std::vector<CompType>& deps);
+
+    EntityID createEntity();
+    EntityID createEntity(const std::vector<CompType>& deps);
+
+    void removeEntity(EntityID id);
+
+    template<class T>
+    void addComponent(CompType type, EntityID id);
+
+    void removeComponent(CompType type, EntityID id);
+
+    template<class T>
+    T* getComponent(CompType type, EntityID id);
+
+    /// <summary>
+    /// Gets the component list of the given EntityID.
+    /// </summary>
+    /// <param name="id">The identifier.</param>
+    /// <returns></returns>
+    std::vector<std::pair<CompType, BaseComponent*>> getComponents(EntityID id);
 
 private:
     std::vector<ComponentList>  mComponents;
@@ -34,3 +57,29 @@ private:
 
 
 #endif // !ENTITY_MANAGER_H_
+
+template<class T>
+inline void EntityManager::addComponent(CompType type, EntityID id)
+{
+    if (mComponents[type][id] != nullptr)
+    {
+        std::cout << "Component already present!" << std::endl;
+        return;
+    }
+
+    static_assert(std::is_base_of<BaseComponent, T>::value, "T must derive from BaseComponent");
+
+    mComponents[type][id].reset(new T());
+
+    // TODO notify processors of new component
+
+}
+
+template<class T>
+inline T * EntityManager::getComponent(CompType type, EntityID id)
+{
+    static_assert(std::is_base_of<BaseComponent, T>::value, "T must derive from BaseComponent");
+
+
+    return static_cast<T*>(mComponents[type][id].get());
+}
